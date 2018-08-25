392fd95ab8e88afb54a5620a584cfdde8775da3a
dev.raviteja <dev.raviteja@gmail.com>
2018-08-26 01:53:06 +0530

    Initial commit

---
+++ app/src/main/java/com/mobilitypirates/raviteja/imagepickerandcropper/MainActivity.java
@@ -0,0 +1,98 @@
+package com.mobilitypirates.raviteja.imagepickerandcropper;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv_image;
+    Button btn_choose_image;
+
+    private static final int REQUEST_CHOOSE_IMAGE = 3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv_image = (ImageView) findViewById(R.id.iv_image);
+        btn_choose_image = (Button) findViewById(R.id.btn_choose_image);
+
+        Glide.with(this).load(R.drawable.images).apply(new RequestOptions().circleCrop()).into(iv_image);
+
+        btn_choose_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Getting the image from device
+                EasyImage.openChooserWithGallery(MainActivity.this, "Choose Picture",
+                        REQUEST_CHOOSE_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // method to handle when the user has selected an image.
+        // when the image has been selected then the image will be redirected to activity
+        // library android-image-picker
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+                CropImage.activity(Uri.fromFile(imageFile))
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .setFixAspectRatio(true)
+                        .start(MainActivity.this);
+            }
+
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                super.onImagePickerError(e, source, type);
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCanceled(EasyImage.ImageSource source, int type) {
+                super.onCanceled(source, type);
+            }
+        });
+
+        // This method works when it's finished from Android-image-picker activity
+        // If result_ok then the image that has been cropped will be entered into the imageview
+        // which we use to use the library glide.
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                Glide.with(this)
+                        .load(new File(resultUri.getPath()))
+                        .apply(new RequestOptions().circleCrop())
+                        .into(iv_image );
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+
+                Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
